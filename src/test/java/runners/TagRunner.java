30a5a0e0a65206e5eb5917191c30a7adbfac4c0b
umitkalemkus <umitkalemkus@yahoo.com>
2024-01-15 22:45:41 +0000

    Jenkinsfile

--- src/test/java/runners/TagRunner.java
+++ src/test/java/runners/TagRunner.java
@@ -2,7 +2,7 @@ package runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+
 import org.junit.runner.RunWith;
 
 
@@ -13,6 +13,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
+        plugin = {"pretty",
+                "html:target/cucumber-reportsAPI.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+
+
+
         features = "src/test/resources/features",
         glue = "stepdefinitions" ,
         tags = "@smoke",
@@ -22,7 +31,7 @@ import org.junit.runner.RunWith;
 
 )
 
-public class TagRunner extends AbstractTestNGCucumberTests {
+public class TagRunner  {
 
 
 }
